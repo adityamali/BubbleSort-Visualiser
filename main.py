56b62ba293137c8d10acf0d16619e24a155bdcc9
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2022-12-22 12:59:40 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((500, 400))
+pygame.display.set_caption("Bubble sort")
+x = 40
+y = 40
+width = 20
+height = [150, 70, 160, 40, 160, 200, 110,
+          99, 33, 20, 130, 159, 140, 190]
+run = True
+
+
+def show(height):
+    for i in range(len(height)):
+        pygame.draw.rect(win, (255, 0, 0), (x + 30 * i, y, width, height[i]))
+
+
+while run:
+    execute = False
+    pygame.time.delay(10)
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    if keys[pygame.K_SPACE]:
+        execute = True
+    if execute == False:
+        win.fill((0, 0, 0))
+        show(height)
+        pygame.display.update()
+    else:
+        for i in range(len(height) - 1):
+            for j in range(len(height) - i - 1):
+                if height[j] > height[j + 1]:
+                    t = height[j]
+                    height[j] = height[j + 1]
+                    height[j + 1] = t
+                win.fill((0, 0, 0))
+                show(height)
+                pygame.time.delay(50)
+                pygame.display.update()
+pygame.quit()
